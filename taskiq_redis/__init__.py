4251647f0028ef16368937f990074a9c5a7a17d6
Pavel Kirilin <win10@list.ru>
2022-08-09 11:39:06 +0400

    Updated taskiq-redis.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- taskiq_redis/__init__.py
+++ taskiq_redis/__init__.py
@@ -1,1 +1,6 @@
 """Package for redis integration."""
+from taskiq_redis.redis_backend import (
+    RedisAsyncResultBackend,
+)
+
+__all__ = ["RedisAsyncResultBackend"]
--- taskiq_redis/taskiq/result_backends/redis/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Redis AsyncResultBackend."""
-from taskiq.result_backends.redis.redis_backend import (
-    RedisAsyncResultBackend,
-)
-
-__all__ = ["RedisAsyncResultBackend"]
