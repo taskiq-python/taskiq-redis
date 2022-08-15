5fa2ef638d62febd69c50c7e3c628987df9c5324
Aleksandr <askiselev00@gmail.com>
2022-08-15 22:00:53 +0300

    Added redis broker.

--- taskiq_redis/__init__.py
+++ taskiq_redis/__init__.py
@@ -1,6 +1,5 @@
 """Package for redis integration."""
-from taskiq_redis.redis_backend import (
-    RedisAsyncResultBackend,
-)
+from taskiq_redis.redis_backend import RedisAsyncResultBackend
+from taskiq_redis.redis_broker import RedisBroker
 
-__all__ = ["RedisAsyncResultBackend"]
+__all__ = ["RedisAsyncResultBackend", "RedisBroker"]
